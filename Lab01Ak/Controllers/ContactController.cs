c2fdd8a257693b96c3a88a92850d036752577ee3
Sroczysk0 <138120680+Sroczysk0@users.noreply.github.com>
2024-10-22 13:12:16 +0200

    33

---
+++ Lab01Ak/Controllers/ContactController.cs
@@ -0,0 +1,47 @@
+﻿using Lab01Ak.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaboratoriumASPNET.Controllers;
+
+public class ContactController : Controller
+{
+    //rozwiazanie tymczasowe
+    static private Dictionary<int, ContactModel> _contacts = new Dictionary<int, ContactModel>(){};
+
+    private static int currentId = 0;
+    // Lista kontaktów
+    public IActionResult Index()
+    {
+        return View(_contacts);
+    }
+    
+    //formularz dodania kontaktu
+    public IActionResult Add()
+    {
+        return View();
+    }
+
+    // odebranie danych z formularza i zapisanie w kontaktach
+    [HttpPost]
+    public IActionResult Add(ContactModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+        model.Id = currentId++;
+        _contacts.Add(model.Id, model);
+        return View("Index", _contacts);
+    }
+
+    public IActionResult Delete(int id)
+    {
+        _contacts.Remove(id);
+        return View("Index", _contacts);
+    }
+
+    public IActionResult Details(int id)
+    {
+        return View(_contacts[id]);
+    }
+}
---
+++ Lab01Ak/Models/ContactModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab01Ak.Models;
+
+public class ContactModel
+{
+    [HiddenInput]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Please enter your first name")]
+    [MaxLength(length: 20, ErrorMessage = "Name too long")]
+    public string FirstName { get; set; }
+    
+    [Required(ErrorMessage = "Please enter your last name")]
+    [MaxLength(length: 50, ErrorMessage = "LastName too long")]
+    public string LastName { get; set; }
+    
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Phone]
+    [RegularExpression("\\d\\d\\d \\d\\d\\d \\d\\d\\d")]
+    public string PhoneNumber { get; set; }
+    
+    [DataType(DataType.Date)]
+    public DateTime BirthDate { get; set; }
+}
