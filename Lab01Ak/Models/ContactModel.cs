b58f6093196ed26491f8e752ff96dbef149450d5
Sroczysk0 <138120680+Sroczysk0@users.noreply.github.com>
2024-10-29 13:17:09 +0100

    ...

--- Lab01Ak/Models/ContactModel.cs
+++ Lab01Ak/Models/ContactModel.cs
@@ -10,19 +10,30 @@ public class ContactModel
     
     [Required(ErrorMessage = "Please enter your first name")]
     [MaxLength(length: 20, ErrorMessage = "Name too long")]
+    [Display(Name = "Imie")]
     public string FirstName { get; set; }
     
     [Required(ErrorMessage = "Please enter your last name")]
     [MaxLength(length: 50, ErrorMessage = "LastName too long")]
+    [Display(Name = "Nazwisko")]
     public string LastName { get; set; }
     
     [EmailAddress]
+    [Display(Name = "Adres Email")]
+
     public string Email { get; set; }
     
     [Phone]
+    [Display(Name = "Numer Telefonu")]
+
     [RegularExpression("\\d\\d\\d \\d\\d\\d \\d\\d\\d")]
     public string PhoneNumber { get; set; }
     
     [DataType(DataType.Date)]
+    [Display(Name = "Data urodzenia")]
+
     public DateTime BirthDate { get; set; }
+    [Display(Name = "Kategoria")]
+
+    public Category Category { get; set; }
 }
