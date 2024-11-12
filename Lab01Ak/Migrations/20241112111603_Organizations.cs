98031528f5b27bd35066a232f322e9e532c7b5c3
Sroczysk0 <138120680+Sroczysk0@users.noreply.github.com>
2024-11-12 13:01:08 +0100

    66

---
+++ Lab01Ak/Migrations/20241112111603_Organizations.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LaboratoriumASPNET.Migrations
+{
+    /// <inheritdoc />
+    public partial class Organizations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OrganizationId",
+                table: "contacts",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "organizations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Regon = table.Column<string>(type: "TEXT", nullable: false),
+                    Nip = table.Column<string>(type: "TEXT", nullable: false),
+                    Address_City = table.Column<string>(type: "TEXT", nullable: true),
+                    Address_Street = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_organizations", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "contacts",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Created", "OrganizationId" },
+                values: new object[] { new DateTime(2024, 11, 12, 12, 16, 3, 14, DateTimeKind.Local).AddTicks(9857), 1 });
+
+            migrationBuilder.InsertData(
+                table: "organizations",
+                columns: new[] { "Id", "Address_City", "Address_Street", "Name", "Nip", "Regon" },
+                values: new object[,]
+                {
+                    { 1, "Kraków", "Św. Filipa 17", "WSEI", "123456", "321321321" },
+                    { 2, "Warszawa", "Wesoła 15", "Famo", "432432", "123123123" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "contacts",
+                columns: new[] { "Id", "BirthDate", "Category", "Created", "Email", "FirstName", "LastName", "OrganizationId", "PhoneNumber" },
+                values: new object[] { 2, new DateTime(2001, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 0, new DateTime(2024, 11, 12, 12, 16, 3, 14, DateTimeKind.Local).AddTicks(9920), "ola@wsei.edu.pl", "Ola", "Nowak", 2, "123123123" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_contacts_OrganizationId",
+                table: "contacts",
+                column: "OrganizationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_contacts_organizations_OrganizationId",
+                table: "contacts",
+                column: "OrganizationId",
+                principalTable: "organizations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_contacts_organizations_OrganizationId",
+                table: "contacts");
+
+            migrationBuilder.DropTable(
+                name: "organizations");
+
+            migrationBuilder.DropIndex(
+                name: "IX_contacts_OrganizationId",
+                table: "contacts");
+
+            migrationBuilder.DeleteData(
+                table: "contacts",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DropColumn(
+                name: "OrganizationId",
+                table: "contacts");
+
+            migrationBuilder.UpdateData(
+                table: "contacts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2024, 11, 5, 12, 52, 2, 238, DateTimeKind.Local).AddTicks(7669));
+        }
+    }
+}
